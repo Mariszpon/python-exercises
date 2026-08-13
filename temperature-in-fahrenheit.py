054a5a621ca15fdb401618f1d6742f3220b5d335
Mariana Pondé <marianahponde@gmail.com>
2026-08-13 20:55:47 -0300

    Create temperature-in-fahrenheit.py

---
+++ temperature-in-fahrenheit.py
@@ -0,0 +1,4 @@
+temperatura = int(input("Qual a temperatura atual? "))
+temperatura_em_fahrenheit = temperatura * 9 / 5 + 32
+temperatura_em_fahrenheit = input(f"Temperatura em fahrenheit:
+{temperatura_em_fahrenheit}")
