16734ad267303651a11a8df1140cf9b3a78e4f40
Mariana Pondé <marianahponde@gmail.com>
2026-08-13 20:59:41 -0300

    Create interval.py

---
+++ interval.py
@@ -0,0 +1,7 @@
+numero = int(input("Escolha um número: "))
+if 50 > numero > 10:
+    print("Seu número está entre 10 e 50.")
+elif numero > 50:
+    print ("Seu número é maior que 50.")
+elif numero < 10:
+    print ("Seu número é menor que 10.")
