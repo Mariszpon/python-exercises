d005f83cd92737ad9f634ea415290e92c8a1d670
Mariana Pondé <marianahponde@gmail.com>
2026-08-13 20:56:27 -0300

    Create test-score.py

---
+++ test-score.py
@@ -0,0 +1,6 @@
+nota_1 = float(input("Qual sua 1º nota? "))
+nota_2 = float(input("Qual sua 2º nota? "))
+nota_3 = float(input("Qual sua 3º nota? "))
+nota_4 = float(input("Qual sua 4º nota? "))
+nota_total = float(nota_1 + nota_2 + nota_3 + nota_4)/4
+nota_total = input(f"Seu total é {nota_total}")
