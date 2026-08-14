3820b702a0e3bcf6012201f2532372a1e8cff6d9
Mariana Pondé <marianahponde@gmail.com>
2026-08-13 21:10:42 -0300

    Delete rating directory

--- rating/evaluation.py
+++
@@ -1,5 +0,0 @@
-nota = float(input("Qual sua nota? "))
-while nota < 0 or nota > 10:
-    print("Nota inválida. Digite novamente.")
-    nota = float(input("Qual sua nota? "))
-print("Nota válida.")
