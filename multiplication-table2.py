ce8bab8a5e6ac5c4f72ebab1da68ef37f5b1be61
Mariana Pondé <marianahponde@gmail.com>
2026-08-13 21:21:19 -0300

    Create multiplication-table2.py

---
+++ multiplication-table2.py
@@ -0,0 +1,8 @@
+inicial = int(input("Digite o valor inicial: "))
+final = int(input("Digite o valor final: "))
+for i in range (inicial, final + 1)
+multiplo = 1
+while multiplo <= 10
+print (f"{i} * {multiplo} = {i * multiplo}")
+multiplo = multiplo + 1
+print()
