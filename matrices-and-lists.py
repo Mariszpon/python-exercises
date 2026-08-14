6020b59929233936a1b80e63bc34829ead1aa917
Mariana Pondé <marianahponde@gmail.com>
2026-08-13 21:34:51 -0300

    Create matrices-and-lists.py

---
+++ matrices-and-lists.py
@@ -0,0 +1,32 @@
+matriz = [
+    [1,2],
+    [3,4]
+]
+print()
+
+# matriz[linha][coluna]
+
+matriz = [
+    [0, 0, 2, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+]
+
+lista = [9, 8, 7, 6]
+for indice in range(4):
+    print(lista[indice])
+
+print()
+
+matriz = [
+    [9, 8],
+    [7, 6]
+]
+for linha in range(2):
+    for coluna in range(2):
+        print (matriz[linha][coluna])
+print()
+for linha in range(2):
+    print(matriz[linha])
+    print()
